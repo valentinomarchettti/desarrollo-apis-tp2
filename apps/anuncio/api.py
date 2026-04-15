6b7217a5d5cfbeb30fa69e5a4607d8a46a5e6a98
Valentino Marchetti <valentinomarchettti@gmail.com>
2026-04-15 16:05:06 -0300

    feat: agregar perform_create y accion tiempo-restante

--- apps/anuncio/api.py
+++ apps/anuncio/api.py
@@ -1,11 +1,21 @@
 from rest_framework import status, viewsets
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.utils import timezone
 
 from rest_framework.generics import (get_object_or_404, ListCreateAPIView, RetrieveUpdateDestroyAPIView)
 from .models import Categoria, Anuncio
 from .serializers import CategoriaSerializer, AnuncioSerializer
 from django.shortcuts import get_object_or_404
+
+from ..usuario.models import Usuario
+
+SEGUNDOS_POR_MINUTO = 60
+MINUTOS_POR_HORA = 60
+HORAS_POR_DIA = 24
+SEGUNDOS_POR_HORA = SEGUNDOS_POR_MINUTO * MINUTOS_POR_HORA
+SEGUNDOS_POR_DIA = SEGUNDOS_POR_HORA * HORAS_POR_DIA
 
 
 # Vistas para Categorias
@@ -95,6 +105,9 @@ class AnuncioListaGenericView(ListCreateAPIView):
     queryset = Anuncio.objects.all()
     serializer_class = AnuncioSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(publicado_por=get_object_or_404(Usuario, id=1))
+
 
 class AnuncioDetalleGenericView(RetrieveUpdateDestroyAPIView):
     queryset = Anuncio.objects.all()
@@ -111,3 +124,53 @@ class CategoriaViewSet(viewsets.ModelViewSet):
 class AnuncioViewSet(viewsets.ModelViewSet):
     queryset = Anuncio.objects.all()
     serializer_class = AnuncioSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(publicado_por=get_object_or_404(Usuario, id=1))
+
+    @action(detail=True, methods=['get'], url_path='tiempo-restante')
+    def tiempo_restante(self, request, pk=None):
+        anuncio = self.get_object()
+
+        if anuncio.fecha_fin is None:
+            return Response(
+                {'detail': 'El anuncio no tiene fecha_fin definida.'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        ahora = timezone.now()
+        diferencia = anuncio.fecha_fin - ahora
+
+        if diferencia.total_seconds() <= 0:
+            return Response(
+                {
+                    'anuncio_id': anuncio.id,
+                    'finalizado': True,
+                    'tiempo_restante': {
+                        'dias': 0,
+                        'horas': 0,
+                        'minutos': 0
+                    }
+                },
+                status=status.HTTP_200_OK
+            )
+
+        total_segundos = int(diferencia.total_seconds())
+
+        #(divmod)Función que guarda el cociente entero, y el resto de la división
+        dias, resto = divmod(total_segundos, SEGUNDOS_POR_DIA)
+        horas, resto = divmod(resto, SEGUNDOS_POR_HORA)
+        minutos = resto // SEGUNDOS_POR_MINUTO
+
+        return Response(
+            {
+                'anuncio_id': anuncio.id,
+                'finalizado': False,
+                'tiempo_restante': {
+                    'dias': dias,
+                    'horas': horas,
+                    'minutos': minutos
+                }
+            },
+            status=status.HTTP_200_OK
+        )
